592eeefd42b51c77d19432874168ff254303801a
nashxH2003 <thurainhein.work@gmail.com>
2024-12-02 17:41:12 +0700

    Git Initiate.

---
+++ lib/route/app_route.dart
@@ -0,0 +1,12 @@
+import 'package:century_art_flutter/features/home/presentation/screens/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
+);
