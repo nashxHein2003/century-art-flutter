dcd1925a4eb8aa62278062a5070a1f3c4f1e7349
nashxH2003 <thurainhein.work@gmail.com>
2024-12-02 17:50:24 +0700

    Added: app theme, responsive framework.

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kBlack = Color(0xFF000000);
+
+final ThemeData appTheme = ThemeData(
+  scaffoldBackgroundColor: kBlack,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:century_art_flutter/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:century_art_flutter/route/app_route.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: router,
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
       debugShowCheckedModeBanner: false,
+      theme: appTheme,
     );
   }
 }
